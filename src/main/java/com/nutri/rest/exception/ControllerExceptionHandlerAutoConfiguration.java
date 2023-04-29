c95d7f24bed7cf00d76d1db3c8af1295c0830a4a
18551A0573 <galla.sathvika2001@gmail.com>
2023-04-29 02:38:52 -0400

    exception

---
+++ src/main/java/com/nutri/rest/exception/ControllerExceptionHandlerAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.nutri.rest.exception;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ControllerExceptionHandlerAutoConfiguration {
+
+   /* @Bean
+    public ControllerExceptionHandler controllerExceptionHandler() {
+        return new ControllerExceptionHandler();
+    }*/
+}
